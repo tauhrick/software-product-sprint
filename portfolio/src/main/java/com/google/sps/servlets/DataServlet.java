cb5a1bcfed660a3a4aa6626013ddd6b6a27fb456
Chirag Thakur <chirag11032000@gmail.com>
2020-07-19 09:36:13 +0530

    Adds comment box

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,19 +14,58 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Chirag!</h1>");
-  }
+    /**
+     * Retrieves comments from database and returns them in JSON format sorted 
+     * in descending order by time when they were added to database.
+     */
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Query query = new Query("comment").addSort("timestamp", SortDirection.DESCENDING);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        PreparedQuery queryResults = datastore.prepare(query);
+        List<String> comments = new ArrayList<>();
+        for (Entity entity : queryResults.asIterable()) {
+            String comment = (String) entity.getProperty("commentBody");
+            comments.add(comment);
+        }
+        response.setContentType("application/json;");
+        response.getWriter().println(new Gson().toJson(comments));
+    }
+
+    /** 
+     * Adds comments to database and redirects to home page.
+     *
+     * A Comment has following attributes:
+     * - body: Body of the comment
+     * - timestamp: Time when the server received comment from the client
+     */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String commentBody = request.getParameter("commentBody");
+        long timestamp = System.currentTimeMillis();
+        Entity commentEntity = new Entity("comment");
+        commentEntity.setProperty("commentBody", commentBody);
+        commentEntity.setProperty("timestamp", timestamp);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(commentEntity);
+        response.sendRedirect("/index.html");
+    }
 }
